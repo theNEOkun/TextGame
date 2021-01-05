070b83d113746428ca094ac4ed305f270630c226
theNEOkun <sven@raswill.se>
2021-01-05 14:59:47 +0100

    Worldupdate
    Updated world, made some changes to be able to output map at the same time as information, and changed spawnlocation.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import json
+import sys
 import Base as b
 from ast import literal_eval
 import world_creator as wc
 
 INVENTORY = dict()
 
-
 RIM, GROUND, PLAYER = '#', '-', 'X'  # base cell states
 STATE_ELDER, STATE_PRIME_ELDER = 'E', 'P'           # cell states used for higher grade
+
+
+def print_to_screen(_status, world, posit):
+    b.clear_console()
+    mapper(world, posit)
+    sys.stdout.write("\n\n")
+    sys.stdout.write("{}".format(_status))
+    sys.stdout.write("\nWhat do you want to do?\n")
 
 
 def get_print_value(_val: str) -> str:
@@ -26,26 +34,27 @@ def get_print_value(_val: str) -> str:
     return "{}{}{}".format(get_state_color(_val), _val, get_state_color('ENDC'))
 
 
-def mapper(mapp, posit):
+def mapper(world, posit):
     _printer = []
     _i = 9
-    for _cell in mapp:  # This only takes care of adding to the list to be printed out
-        if mapp[_cell]:
-            if mapp[_cell] == posit:
-                _printer.append(get_print_value(PLAYER))
+    for _cell in world:
+        if world[_cell]["mapping"] != "#":
+            if world[_cell] == posit:
+                _printer.append(get_print_value(PLAYER) + " ")
             else:
-                _printer.append(get_print_value(mapp[_cell]["part"]))
-        else:  # This puts everything into the list
-            _printer.append(get_print_value(RIM))
+                _printer.append(get_print_value(world[_cell]["mapping"]) + " ")
+        else:
+            _printer.append(get_print_value(RIM) + " ")
 
     while _i < len(_printer):
         _printer.insert(_i, "\n")  # Adds linebreaks where needed.
         _i += 9 + 1
 
-    b.progress(''.join(_printer))  # Prints it out
+    sys.stdout.write(''.join(_printer))  # Prints it out
 
 
 def open_file():
+
     the_world = dict()
 
     with open("_RESOURCES/world.json", "r") as infile:
@@ -54,21 +63,11 @@ def open_file():
     for key, value in data.items():
         the_world[literal_eval(key)] = data[key]
 
-    the_map = dict()
-
-    with open("_RESOURCES/mapp.json", "r") as infile:
-        data = json.load(infile)
-
-    for key, value in data.items():
-        the_map[literal_eval(key)] = data[key]
-
-    return the_world, the_map
+    return the_world
 
 
-def place_description(world, mapp, posit):
-    b.clear_console()
-    mapper(mapp, posit)
-    b.progress(world[posit]["description"]["place"] + "\n")
+def place_description(world, posit):
+    return world[posit]["description"]["place"] + "\n"
 
 
 def look(square, in_put):
@@ -79,42 +78,34 @@ def look(square, in_put):
 
         if "at" in in_put:
             try:
-                b.progress(possible_interactions[inputs[2]] + "\n")
+                return possible_interactions[inputs[2]] + "\n"
             except KeyError:
-                b.progress("You can't do that\n")
+                return "You can't do that\n"
         if "around" in in_put:
-            b.clear_console()
-            b.fancy()
             output = str()
             output += square["description"]["place"] + "\n"
             output += "You see:\n"
             for keys, values in square["items"].items():
                 output += "{}\n".format(values["desc"])
-            b.progress("{:^}\n".format(output))
+            return "{:^}\n".format(output)
         if inputs[1] in cardinals:
             try:
-                b.progress(square["directions"][inputs[1]] + "\n")
+                return square["directions"][inputs[1]] + "\n"
             except KeyError:
-                b.progress("Where did you want to look?\n")
+                return "Where did you want to look?\n"
 
 
-def walker(world, mapp, posit, in_put):
+def walker(world, posit, in_put):
     inputs = in_put.split(" ")
-    try:
-        direction = tuple(world[posit]["neighbours"][inputs[1]]["coord"])
-        if world[direction]:
-            if world[posit]["neighbours"][inputs[1]]["walk"] == "yes":
-                place_description(world, mapp, direction)
-                return direction
-            else:
-                b.progress("Can't walk there because it's a {}\n".format(world[posit]["neighbours"][inputs[1]]["why"]))
-                return posit
+    direction = tuple(world[posit]["neighbours"][inputs[1]]["coord"])
+    if world[direction]:
+        if world[posit]["neighbours"][inputs[1]]["walk"] == "yes":
+            return direction, place_description(world, direction)
         else:
-            b.progress("There's no reason to go there\n")
-            return posit
-    except KeyError:
-        b.progress("Where did you want to go?\n")
-        return posit
+            return posit, ("Can't walk there because {}\n"
+                           .format(world[posit]["neighbours"][inputs[1]]["why"]))
+    else:
+        return posit, "There's no reason to go there\n"
 
 
 def pickup(square, in_put):
@@ -128,35 +119,39 @@ def pickup(square, in_put):
         b.progress("What did you want to pickup?\n")
 
 
-def inputter(world, mapp, posit):
+def inputter(world, posit):
     in_put = str()
+    _status = str()
     while in_put != "exit":
         square = world[posit]
-        b.progress(str(posit) + "\n")
-        b.progress("What do you want to do?\n")
         in_put = input()
         if "look" in in_put:
-            look(square, in_put)
+            _status = look(square, in_put)
         if "walk" in in_put or "go" in in_put:
-            posit = walker(world, mapp, posit, in_put)
+            posit, _status = walker(world, posit, in_put)
         if "pickup" in in_put:
             pickup(square, in_put)
         if "open" in in_put:
-            print(INVENTORY)
+            _status = INVENTORY
+        print_to_screen(_status, world, posit)
     else:
         b.clear_console()
-        b.progress("Goodbye")
+        sys.stdout.write("Goodbye")
 
 
 def main_part():
-    world, mapp = open_file()
-    posit = (4, 0)
+    world = open_file()
     b.set_console()
-    place_description(world, mapp, posit)
-    inputter(world, mapp, posit)
+    posit = (0, 4)
+    print_to_screen(place_description(world, posit), world, posit)
+    inputter(world, posit)
 
 
 if __name__ == '__main__':
-    main_part()
-    # wc.creator()
+    sys.stdout.write("Start, or reload world")
+    interput = input("What do you want to do?")
+    if interput == "start":
+        main_part()
+    if interput == "reload world":
+        wc.creator()
     # wc.mapping()
--- world_creator.py
+++ world_creator.py
@@ -6,62 +6,65 @@ RESOURCES = Path(__file__) / "_RESOURCES/"
 
 
 def mapping():
-    x_axis = range(9)
-    y_axis = range(9)
+    x_axis = 9
+    y_axis = 9
     mapp = dict()
 
-    grid = itertools.product(x_axis, y_axis)
+    grid = itertools.product(range(x_axis), range(y_axis))
 
     for each in grid:
         x_, y_ = each
-        if x_ == 0 or y_ == 0 or x_ + 1 == 9 or y_ + 1 == 9:  # If it is in any edge point
+        if x_ == 0 or y_ == 0 or x_ + 1 == y_axis or y_ + 1 == x_axis:  # If it is in any edge point
             mapp[each] = None  # Set to none and skip onwards
             continue
         else:
             mapp[each] = {"part": "-"}
 
-    mapp[(4, 0)] = {"part": "-"}
+    mapp[(0, 4)] = {"part": "-"}
 
     with open("_RESOURCES/mapp.json", "w") as outfile:
         json.dump({str(k): v for k, v in mapp.items()}, outfile, indent=4)
 
 
 def creator():
-    x_axis = range(9)
-    y_axis = range(9)
+    x_axis = 9
+    y_axis = 9
     world = dict()
 
-    grid = itertools.product(x_axis, y_axis)
+    grid = itertools.product(range(x_axis), range(y_axis))
 
     for each in grid:
         x_, y_ = each
-        if x_ == 0 or y_ == 0 or x_ + 1 == y_axis or y_ + 1 == x_axis:  # If it is in any edge point
-            world[each] = None  # Set to none and skip onwards
-            continue
-        else:
-            world[each] = {"description": {"place": "placeholder"}}
-            world[each].update({"directions":
-                                {"north": "placeholder",
-                                 "south": "placeholder",
-                                 "west": "placeholder",
-                                 "east": "placeholder"
+
+        world[each] = {"description": {"place": "placeholder"}}
+        world[each].update({"directions":
+                            {"north": "placeholder",
+                             "south": "placeholder",
+                             "west": "placeholder",
+                             "east": "placeholder"
+                             },
+                            "items":
+                                {"item1": {"desc": "placeholder",
+                                           "pickup": "no"},
+                                 "item2": {"desc": "placeholder",
+                                           "pickup": "yes"},
+                                 "item3": {"desc": "placeholder",
+                                           "pickup": "no"}
                                  },
-                                "items":
-                                    {"item1": {"desc": "placeholder",
-                                               "pickup": "no"},
-                                     "item2": {"desc": "placeholder",
-                                               "pickup": "yes"},
-                                     "item3": {"desc": "placeholder",
-                                               "pickup": "no"}
-                                     },
-                                "interactions":
-                                    {"interact1": "placeholder",
-                                     "interact2": "placeholder",
-                                     "interact3": "placeholder"
-                                     },
-                                "neighbours": calc_neigh(each)
-                                }
-                               )
+                            "interactions":
+                                {"interact1": "placeholder",
+                                 "interact2": "placeholder",
+                                 "interact3": "placeholder"
+                                 },
+                            "neighbours": calc_neigh(each),
+                            "mapping": "-"
+                            }
+                           )
+        if x_ == 0 or y_ == 0 or x_ + 1 == y_axis or y_ + 1 == x_axis:  # If it is in any edge point
+            if world[each] == (4, 0):
+                world[(0, 4)]["mapping"] = "-"
+            world[each]["mapping"] = "#"  # Set to none and skip onwards
+            continue
 
     with open("_RESOURCES/world.json", "w") as outfile:
         json.dump({str(k): v for k, v in world.items()}, outfile, indent=4)
